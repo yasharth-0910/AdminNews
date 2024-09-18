824ccc4fa0590f0dccc8b0bd23cdc3360f3d016c
Yasharth Singh <yasharthsingh0910@gmail.com>
2024-09-18 23:53:01 +0530

    AdminNews

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -2,12 +2,18 @@ import axios from 'axios';
 
 // Access the API key from environment variables
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
-const BASE_URL = 'https://newsapi.org/v2/top-headlines';
+const BASE_URL = 'https://newsapi.org/v2/everything';
 
-// Function to fetch articles based on category
-export const fetchArticles = async (category) => {
+// Function to fetch articles based on query
+export const fetchArticles = async (query) => {
   try {
-    const response = await axios.get(`${BASE_URL}?category=${category}&apiKey=${API_KEY}`);
+    const response = await axios.get(`${BASE_URL}`, {
+      params: {
+        q: query,
+        apiKey: API_KEY,
+        // You can add other optional parameters here if needed
+      }
+    });
     return response.data.articles;
   } catch (error) {
     console.error('Error fetching articles:', error);
